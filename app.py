65db5588765405b18a5c2c9929f113e46b6fc77c
Sean Stapleton <seanstapleton100@gmail.com>
2017-09-24 00:25:18 -0400

    update

--- app.py
+++ app.py
@@ -60,6 +60,21 @@ def load_labels(label_file):
 # HTTPRequestHandler class
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
+  # GET
+  def do_GET(self):
+    # Send response status code
+    self.send_response(200)
+
+    # Send headers
+    self.send_header('Content-type','text/html')
+    self.end_headers()
+
+    # Send message back to client
+    message = "Hello world!"
+    # Write content as utf-8 data
+    self.wfile.write(bytes(message, "utf8"))
+    return
+
   def do_POST(self):
     length = int(self.headers['Content-Length'])
     post_data = parse_qs(self.rfile.read(length).decode('utf-8'))
@@ -110,28 +125,12 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     self.wfile.write(bytes(message, "utf8"))
     return
 
-
-    # GET
-    def do_GET(self):
-      # Send response status code
-      self.send_response(200)
-
-      # Send headers
-      self.send_header('Content-type','text/html')
-      self.end_headers()
-
-      # Send message back to client
-      message = "Hello world!"
-      # Write content as utf-8 data
-      self.wfile.write(bytes(message, "utf8"))
-      return
-
 def run():
   print('starting server...')
 
   # Server settings
   # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-  server_address = ('127.0.0.1', 8080)
+  server_address = ('127.0.0.1', 3000)
   httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
   print('running server...')
   httpd.serve_forever()
